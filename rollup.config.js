cd4459bd5cec8d4d950bf76df2479c6974bebff1
linjielinlin <993353454@qq.com>
2023-01-12 11:19:45 +0800

    feat: up rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,16 @@
-// import typescript from 'rollup-plugin-typescript'
-// import dts from 'rollup-plugin-dts'
-const typescript = require('rollup-plugin-typescript')
-const dts = require('rollup-plugin-dts')
+import typescript from '@rollup/plugin-typescript'
 
 // import { terser } from 'rollup-plugin-terser'
 const outputPath = './build'
-const getDts = (name) => {
-  return {
-    input: `./${name}.ts`,
-    output: [
-      {
-        file: `${outputPath}/${name}.d.ts`,
-        format: 'es',
-      },
-    ],
-    plugins: [dts()],
-  }
-}
+
 export default [
   {
     input: './src/index.ts',
     output: [
+      // {
+      //   file: outputPath + '/index.js',
+      //   format: 'esm',
+      // },
       {
         file: outputPath + '/index.cjs',
         format: 'cjs',
@@ -28,14 +18,10 @@ export default [
     ],
     plugins: [
       typescript({
-        exclude: 'node_modules/**',
-        typescript: require('typescript'),
+        compilerOptions: {
+          declarationDir: '',
+        },
       }),
-      // 压缩
-      // terser(),
     ],
-    // sourcemap: true,
   },
-  // d.ts
-  // getDts('index'),
 ]
