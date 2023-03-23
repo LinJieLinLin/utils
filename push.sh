768bcad6c9bc302401edfebd9656fc2beb38067d
LinJieLinLin <993353454@qq.com>
2023-03-23 16:57:23 +0800

    ci: up -1

--- push.sh
+++ push.sh
@@ -21,14 +21,12 @@ case $option in
   ;;
 2)
   # 推送 tag 到远程仓库
-  if [ -n "$(git status --porcelain)" ]; then
+  if git status | grep -q "ahead"; then
     git push origin
+    git push github
   fi
   git push origin $latest_tag
   if git remote | grep -q "github"; then
-    if [ -n "$(git status --porcelain)" ]; then
-      git push github
-    fi
     git push github $latest_tag
   fi
   echo "tag: $latest_tag at $current_time on branch $branch_name"
