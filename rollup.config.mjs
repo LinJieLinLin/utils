67c33f05fc393d770f5fdb1934bcdc59836b75b9
LinJieLinLin <993353454@qq.com>
2024-02-27 23:58:18 +0800

    fix: es6 module

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,24 +4,24 @@ const outputPath = './build'
 const inputFiles = glob.sync('src/**/*.ts')
 
 export default [
-  {
-    input: inputFiles,
-    output: {
-      dir: outputPath,
-      format: 'esm',
-      preserveModulesRoot: 'src',
-      preserveModules: true,
-    },
-    plugins: [
-      typescript({
-        compilerOptions: {
-          declaration: true,
-          declarationDir: outputPath,
-          outDir: outputPath,
-        },
-      }),
-    ],
-  },
+  // {
+  //   input: inputFiles,
+  //   output: {
+  //     dir: outputPath,
+  //     format: 'esm',
+  //     preserveModulesRoot: 'src',
+  //     preserveModules: true,
+  //   },
+  //   plugins: [
+  //     typescript({
+  //       compilerOptions: {
+  //         declaration: true,
+  //         declarationDir: outputPath,
+  //         outDir: outputPath,
+  //       },
+  //     }),
+  //   ],
+  // },
   {
     input: './src/index.ts',
     output: [
